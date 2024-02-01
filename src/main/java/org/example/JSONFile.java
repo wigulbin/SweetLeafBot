ff550fd3fd813961c05061db324e5d3475a0e8b6
wigulbin <wigulbin@gmail.com>
2024-01-31 22:35:48 -0500

    Update file retrieving api to handle packaged resource files better

--- src/main/java/org/example/JSONFile.java
+++ src/main/java/org/example/JSONFile.java
@@ -1,16 +1,24 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class JSONFile {
     private static final String keys = "";
 
+    private static final Logger log = LoggerFactory.getLogger(JSONFile.class);
     public static String getJSONValueFromFile(String key, String resourceName) {
         try
         {
@@ -32,7 +40,7 @@ public class JSONFile {
         }
         catch (Exception e)
         {
-            e.printStackTrace();
+            log.error("JSON Parse error", e);
         }
 
         return new HashMap<>();
@@ -42,7 +50,8 @@ public class JSONFile {
         URL resource = JSONFile.class.getClassLoader().getResource(resourceName);
         if(resource != null) {
             String path =resource.getPath();
-            if(path.startsWith("/"))
+            log.info("Path: " + path);
+            if(path.startsWith(File.separator))
                 path = path.substring(1);
             return path;
         }
@@ -50,11 +59,27 @@ public class JSONFile {
         return "";
     }
 
+    public static String getResourcePath2(String resourceName) {
+        try(InputStream inputStream = JSONFile.class.getClassLoader().getResourceAsStream(resourceName);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))){
+            return reader.lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+        }catch (Exception e){
+            log.error("File error", e);
+        }
+
+        return "";
+    }
+
     public static String getJSONString(String fileName){
         try{
-            return Files.readString(Path.of(getResourcePath(fileName)));
+            String filePath = getResourcePath(fileName);
+            log.info("File Path: " + filePath);
+            Path path = Path.of(getResourcePath(fileName));
+
+            return Files.readString(path);
         } catch (Exception e){
-            System.out.println(e);
+            log.error("JSON Parse error", e);
         }
 
         return "";
--- src/main/java/org/example/Recipes.java
+++ src/main/java/org/example/Recipes.java
@@ -1,7 +1,10 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +12,7 @@ import java.util.List;
 public class Recipes implements Serializable {
     List<Recipe> recipes;
 
+    private static final Logger log = LoggerFactory.getLogger(Recipes.class);
 
     private static final List<Recipe> RECIPE_LIST = new ArrayList<>();
 
@@ -22,13 +26,13 @@ public class Recipes implements Serializable {
 
     public static void loadRecipesFromFile() {
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonString = JSONFile.getJSONString("recipe.json");
+        String jsonString = JSONFile.getResourcePath2("recipe.json");
 
         try{
             Recipes recipes = objectMapper.readValue(jsonString, Recipes.class);
             if(recipes != null) RECIPE_LIST.addAll(recipes.getRecipes());
         } catch (Exception e){
-            System.out.println(e);
+            log.error("Error reading recipe json", e);
         }
     }
 
